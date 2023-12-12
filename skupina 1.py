64699fcb5eabbcfaa22e8a4e16a98f48d5ed7a80
Gustav Flachs <Gustav.Flachs@student.souepl.cz>
2023-12-12 13:49:13 +0100

    commit

---
+++ skupina 1.py
@@ -0,0 +1,42 @@
+x = input ("Zadejte hodnotu x: ")
+y = input ("Zadejte hodnotu y: ")
+
+x = int(x)
+y = int(y)
+print ("Pro sčíání zadejte +")
+print ("Pro odčítání zadejte -")
+print ("Pro násobení zadejte *")
+print ("Pro dělení zadejte /")
+print ("Pro mocnění zadejte **")
+print ("Pro odmocnění zadejte /*")
+
+znaménko = input ("Zvolte znaménko")
+
+if znaménko == "+":
+    výsledek = x + y
+    výsledek = str(výsledek)
+    print (x+y)
+elif znaménko == "-":
+    výsledek = x - y
+    výsledek = str(výsledek)
+    print (x-y)
+elif znaménko == "*":
+    výsledek = x * y
+    výsledek = str(výsledek)
+    print (x*y)
+elif znaménko == "/":
+    if y != 0:
+        výsledek = x / y
+        výsledek = str(výsledek)
+        print (x/y)
+    else:
+        print ("co dělíš nulou co chceš bombu?")
+elif znaménko == "**":
+    výsledek = x ** y
+    výsledek = str(výsledek)
+    print (x**y)
+elif znaménko == "/*":
+    výsledek = x **(1/y)
+    výsledek = str(výsledek)
+    print (x **(1/y))
+print ("výsledek je: " + výsledek)
